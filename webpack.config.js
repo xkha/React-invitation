1a43be2542f3e3cc66d750cf1b8dd879477a99b5
xkha <andreas311089@gmail.com>
2015-06-16 15:30:59 +0300

    fix live reload and add babel es6

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,33 @@
+var path = require('path');
 var webpack = require('webpack');
+var config = require('./src/libs/config');
 
 module.exports = {
+  devtool: 'eval',
   entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/dev-server',
-    './src/app/app.js'
+    'webpack-dev-server/client?http://localhost:' + config.get('server-port'),
+    'webpack/hot/only-dev-server',
+    './src/app/app'
   ],
   output: {
-    path: __dirname,
-    filename: 'bundle.js',
-    publicPath: '/build/'
+    path: path.join(__dirname, 'build'),
+    filename: 'app.js',
+    publicPath: '/src/'
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.ResolverPlugin(new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
   ],
   resolve: {
+    root: [path.join(__dirname, "bower_components")],
     extensions: ['', '.js', '.jsx']
   },
   module: {
-    loaders: [ { test: /\.jsx?$/, loader: 'jsx-loader' } ]
-  },
-  externals: {
-    'showdown': 'window.Showdown'
+    loaders: [{
+      test: /\.jsx?$/,
+      loaders: ['react-hot', 'babel'],
+      include: path.join(__dirname, 'src')
+    }]
   }
 };
