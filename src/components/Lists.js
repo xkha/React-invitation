ca7d5906c637b0945fd48ece5c5122cbacab1a26
apys <alexandrapyshko@gmail.com>
2015-06-20 23:46:30 +0300

    List component added

---
+++ src/components/Lists.js
@@ -0,0 +1,99 @@
+/**
+ * Created by latr on 20.06.2015.
+ */
+import React from 'react';
+import { Paper, List, ListItem, Avatar, FontIcon } from 'material-ui'
+
+import $ from 'jquery';
+
+
+import mui from 'material-ui';
+var Colors = mui.Styles.Colors;
+var ThemeManager = new mui.Styles.ThemeManager();
+
+export default React.createClass({
+
+    childContextTypes: {
+        muiTheme: React.PropTypes.object
+    },
+
+    getChildContext() {
+        return {
+            muiTheme: ThemeManager.getCurrentTheme()
+        };
+    },
+
+    getStyles() {
+        var style = {
+
+            mainContainer: {
+
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                width: '630px'
+
+            },
+
+            paper: {
+
+                width: '630px',
+                float: 'left',
+                marginBottom: '50px'
+            }
+        };
+
+        return style;
+    },
+
+    render() {
+        var styles = this.getStyles();
+        return (
+            <div style={styles.mainContainer}>
+                <Paper zDepth={2} style={styles.paper}>
+                    <List subheader="Users">
+                        <ListItem
+                            leftAvatar={<Avatar
+                                icon={<FontIcon className="icon-VK" />}
+                                color={Colors.blue300}
+                                backgroundColor={Colors.indigo900} />}
+                            rightIcon={<FontIcon className="icon-VK" />}>
+                            Brendan Lim
+                        </ListItem>
+                        <ListItem
+                            leftAvatar={<Avatar
+                                icon={<FontIcon className="icon-VK" />}
+                                color={Colors.blue300}
+                                backgroundColor={Colors.indigo900} />}
+                            rightIcon={<FontIcon className="icon-VK" />}>
+                            Eric Hoffman
+                        </ListItem>
+                        <ListItem
+                            leftAvatar={<Avatar
+                                icon={<FontIcon className="icon-VK" />}
+                                color={Colors.blue300}
+                                backgroundColor={Colors.indigo900} />}
+                            rightIcon={<FontIcon className="icon-VK" />}>
+                            Grace Ng
+                        </ListItem>
+                        <ListItem
+                            leftAvatar={<Avatar
+                                icon={<FontIcon className="icon-VK" />}
+                                color={Colors.blue300}
+                                backgroundColor={Colors.indigo900} />}
+                            rightIcon={<FontIcon className="icon-VK" />}>
+                            Kerem Suer
+                        </ListItem>
+                        <ListItem
+                            leftAvatar={<Avatar
+                                icon={<FontIcon className="icon-VK" />}
+                                color={Colors.blue300}
+                                backgroundColor={Colors.indigo900} />}
+                            rightIcon={<FontIcon className="icon-VK" />}>
+                            Raquel Parrado
+                        </ListItem>
+                    </List>
+                </Paper>
+            </div>
+        );
+    }
+});
