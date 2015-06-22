4ae26a605e1d9af0ec7277cbb1bc3c77316810bd
xkha <andreas311089@gmail.com>
2015-06-22 10:13:29 +0300

    fix

--- src/app/app.js
+++ src/app/app.js
@@ -1,41 +1,44 @@
 import React from 'react';
-import CommentBox from './../components/CommentBox';
 import InjectTapEventPlugin from 'react-tap-event-plugin';
+
 import LoginForm from './../components/LoginForm.js';
 import Registration from './../components/Registration.js';
 import Lists from './../components/Lists.js';
+import CommentBox from './../components/CommentBox';
 
 var Router = require('react-router');
 var Route = Router.Route;
-
-InjectTapEventPlugin();
-
-window.React = React;
-
-/*React.render(<CommentBox url='/api/comments' pollInterval={10000} />, document.getElementById('content'));
-React.render(<LoginForm />, document.getElementById('login'));
-React.render(<Registration />, document.getElementById('reg'));
-React.render(<Lists />, document.getElementById('list'));*/
-
-var routes = (
-    <Route handler={App}>
-        <Route path="about" handler={LoginForm}/>
-        <Route path="inbox" handler={Registration}/>
-    </Route>
-);
-
 var RouteHandler = Router.RouteHandler;
 
+InjectTapEventPlugin();
+window.React = React;
+
 var App = React.createClass({
-    render () {
+    render() {
         return (
             <div>
-                <h1>App</h1>
                 <RouteHandler/>
             </div>
         )
     }
 });
+
+var Comment = React.createClass({
+    render() {
+        return (
+            <CommentBox url='/api/comments' pollInterval={10000} />
+        )
+    }
+});
+
+var routes = (
+    <Route path="/" handler={App}>
+        <Route path="login" handler={LoginForm}/>
+        <Route path="registration" handler={Registration}/>
+        <Route path="comment" handler={Comment}/>
+        <Route path="list" handler={Lists}/>
+    </Route>
+);
 
 Router.run(routes, Router.HashLocation, (Root) => {
     React.render(<Root/>, document.body);
