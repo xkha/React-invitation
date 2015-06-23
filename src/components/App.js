1b2dbc44c6cc76c9c09008fd5b299839a05b1370
apys <alexandrapyshko@gmail.com>
2015-06-23 14:22:01 +0300

    right nav added

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Router from 'react-router';
 
-import { AppBar, LeftNav, MenuItem } from 'material-ui';
-import LoginIcon from './../components/LoginIcon';
+import { AppBar, LeftNav, MenuItem, Avatar, FontIcon } from 'material-ui';
 import mui from 'material-ui';
+var {Typography, Colors, Spacing} = mui.Styles;
 
 var ThemeManager = new mui.Styles.ThemeManager();
 
@@ -19,6 +19,11 @@ let menuItems = [
     }
 ];
 
+let rightMenuItems = [
+    { route: 'profile', text: 'Profile' },
+    { route: 'logout', text: 'Logout' }
+];
+
 var App = React.createClass({
     childContextTypes: {
         muiTheme: React.PropTypes.object
@@ -28,23 +33,48 @@ var App = React.createClass({
             muiTheme: ThemeManager.getCurrentTheme()
         };
     },
+    getStyles() {
+        return {
+            cursor: 'pointer',
+            //.mui-font-style-headline
+            fontSize: '24px',
+            color: Typography.textFullWhite,
+            lineHeight: Spacing.desktopKeylineIncrement + 'px',
+            fontWeight: Typography.fontWeightLight,
+            backgroundColor: Colors.cyan500,
+            paddingLeft: Spacing.desktopGutter,
+            paddingTop: '0px',
+            marginBottom: '8px'
+        };
+    },
     render() {
         return (
             <div>
                 <AppBar onLeftIconButtonTouchTap={this._onLeftIconButtonTouchTap}
                         title='React invitation'
-                        iconElementRight ={<LoginIcon/>}/>
+                        iconElementRight={<Avatar style={{cursor: 'pointer'}} onTouchTap={this._onRightButtonTouchTap}>A</Avatar>}/>
                 <LeftNav ref='leftNav' docked={false}
                          menuItems={menuItems}
                          title='React invitation'
                          selectedIndex={this._getSelectedIndex()}
                          onChange={this._onLeftNavChange} />
+                <LeftNav ref='rightNav' docked={false}
+                    menuItems={rightMenuItems}
+                    header={<div style={this.getStyles()}>
+                        Signed in as apys
+                    </div>}
+                    selectedIndex={this._getSelectedIndex()}
+                    onChange={this._onRightNavChange}
+                    openRight={true}/>
                 <RouteHandler/>
             </div>
         )
     },
     _onLeftIconButtonTouchTap() {
         this.refs.leftNav.toggle();
+    },
+    _onRightButtonTouchTap() {
+        this.refs.rightNav.toggle();
     },
     _getSelectedIndex() {
         let currentItem;
@@ -55,6 +85,9 @@ var App = React.createClass({
         }
     },
     _onLeftNavChange(e, key, payload) {
+        this.context.router.transitionTo(payload.route);
+    },
+    _onRightNavChange(e, key, payload) {
         this.context.router.transitionTo(payload.route);
     }
 });
