078792384657280fa17d73f587733e10ab07b835
apys <alexandrapyshko@gmail.com>
2015-06-25 10:38:59 +0300

    home page updated

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -26,16 +26,20 @@ export default React.createClass({
         return {
             root: {
                 backgroundColor: Colors.cyan500,
-                overflow: 'hidden'
+                overflow: 'hidden',
+                margin: '0 auto',
+                boxSizing: 'border-box',
+                padding: '72px 24px'
             },
             svgLogo: {
-                marginLeft: (window.innerWidth * 0.5) - 150 + 'px',
-                width: '300px'
+                marginLeft: (window.innerWidth * 0.5) - 174 + 'px'
             },
             tagline: {
-                margin: '16px auto 0 auto',
+
                 textAlign: 'center',
-                maxWidth: '575px'
+                maxWidth: '575px',
+                marginTop: '32px',
+                margin: '0 auto'
             },
             label: {
                 color: ThemeManager.palette.primary1Color
@@ -49,33 +53,29 @@ export default React.createClass({
             h1: {
                 color: Colors.darkWhite,
                 fontWeight: Typography.fontWeightLight,
-                fontSize: '56'
+                fontSize: '56px',
+                fontFamily: 'Roboto',
+                lineHeight: '20px',
+                wordSpacing: '0px',
+                margin: '0px auto 0px auto'
             },
             h2: {
                 //.mui-font-style-title
-                fontSize: '20px',
-                lineHeight: '28px',
-                paddingTop: '19px',
-                marginBottom: '13px',
-                letterSpacing: '0'
-            },
-            nowrap: {
-                whiteSpace: 'nowrap'
-            },
-            taglineWhenLarge: {
-                marginTop: '32px'
-            },
-            h1WhenLarge: {
-                fontSize: '56px'
-            },
-            h2WhenLarge: {
+                letterSpacing: '0',
                 //.mui-font-style-headline;
                 fontSize: '24px',
                 lineHeight: '32px',
                 paddingTop: '16px',
-                marginBottom: '12px'
-            },
+                marginBottom: '12px',
+                marginTop: '0px',
+                color: 'rgba(255, 255, 255, 0.870588)',
+                fontWeight: 300,
+                fontFamily: 'Roboto'
 
+            },
+            nowrap: {
+                whiteSpace: 'nowrap'
+            },
             rootNext: {
                 backgroundColor: Colors.grey200
             },
@@ -122,24 +122,15 @@ export default React.createClass({
                     <div style={styles.tagline}>
                         <h1 style={styles.h1}>React Invitation</h1>
                         <h2 style={styles.h2}>
-                            A Set of React Components
-                            <span style={styles.nowrap}>
-                                that Implement</span>
-                            <span style={styles.nowrap}>
-                                Google&apos;s Material Design</span>
+                            A Set of React Components <span style={styles.nowrap}>
+                            that Implement</span> <span style={styles.nowrap}>
+                            Google&apos;s Material Design</span>
                         </h2>
-                        <RaisedButton
-                            className="demo-button"
-                            label="Demo"
-                            style={styles.demoStyle}
-                            labelStyle={styles.label}/>
-                        <RaisedButton
-                            className="github-button"
-                            label="GitHub"
-                            linkButton={true}
-                            href="https://github.com/callemall/material-ui"
-                            style={styles.githubStyle}
-                            labelStyle={styles.label}/>
+                        <div>
+                            <RaisedButton label="Demo" labelStyle={styles.label}/> <RaisedButton label="GitHub" labelStyle={styles.label}/>
+
+                        </div>
+
                     </div>
                 </div>
             </div>
